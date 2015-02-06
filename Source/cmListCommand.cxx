c6c6b26accc13ad50cad5130e44d06d52d6f2c1f
Stephen Kelly <steveire@gmail.com>
2015-02-06 19:01:24 +0100

    Revert "cmListCommand: Use std::find algorithm for FIND subcommand."
    This reverts commit 7b2c2fb83d9973fce37864827024ec4f07de8684.

--- Source/cmListCommand.cxx
+++ Source/cmListCommand.cxx
@@ -287,14 +287,18 @@ bool cmListCommand::HandleFindCommand(std::vector<std::string> const& args)
     return true;
     }
 
-  std::vector<std::string>::iterator it =
-      std::find(varArgsExpanded.begin(), varArgsExpanded.end(), args[2]);
-  if (it != varArgsExpanded.end())
+  std::vector<std::string>::iterator it;
+  unsigned int index = 0;
+  for ( it = varArgsExpanded.begin(); it != varArgsExpanded.end(); ++ it )
     {
-    char indexString[32];
-    sprintf(indexString, "%zu", std::distance(varArgsExpanded.begin(), it));
-    this->Makefile->AddDefinition(variableName, indexString);
-    return true;
+    if ( *it == args[2] )
+      {
+      char indexString[32];
+      sprintf(indexString, "%d", index);
+      this->Makefile->AddDefinition(variableName, indexString);
+      return true;
+      }
+    index++;
     }
 
   this->Makefile->AddDefinition(variableName, "-1");

df38fac03da0d9c05d3c613f40f1421088ba8b66
Stephen Kelly <steveire@gmail.com>
2015-02-11 09:30:49 +0100

    Remove unused variable.

--- Source/cmMacroCommand.cxx
+++ Source/cmMacroCommand.cxx
@@ -254,7 +254,6 @@ IsFunctionBlocked(const cmListFileFunction& lff, cmMakefile &mf,
     if (!this->Depth)
       {
       std::string name = this->Args[0];
-      std::vector<std::string>::size_type cc;
       name += "(";
       if (!this->Args.empty())
         {

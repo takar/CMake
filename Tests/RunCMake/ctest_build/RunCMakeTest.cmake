ade4ad327a1a7e061a879c8f327af4308ae636fc
Zack Galbreath <zack.galbreath@kitware.com>
2015-02-25 14:44:33 -0500

    tolerate spaces in path for RunCMake.ctest_build

--- Tests/RunCMake/ctest_build/RunCMakeTest.cmake
+++ Tests/RunCMake/ctest_build/RunCMakeTest.cmake
@@ -12,6 +12,6 @@ run_ctest_build(BuildQuiet QUIET)
 set(CMAKELISTS_EXTRA_CODE "
 enable_language(CXX)
 add_executable(compile_error
- ${RunCMake_SOURCE_DIR}/../../CTestTestFailure/badCode.cxx)")
+ \"${RunCMake_SOURCE_DIR}/../../CTestTestFailure/badCode.cxx\")")
 run_ctest_build(BuildCompileError TARGET compile_error)
 set(CMAKELISTS_EXTRA_CODE "")

60fdb9ad9768e9e08f435fe548d554a858330c8e
Stephen Kelly <steveire@gmail.com>
2015-04-11 16:52:07 +0200

    fixup! Introduce cmState class.

--- Source/cmState.cxx
+++ Source/cmState.cxx
@@ -1,4 +1,14 @@
+/*============================================================================
+  CMake - Cross Platform Makefile Generator
+  Copyright 2015 Stephen Kelly <steveire@gmail.com>
 
+  Distributed under the OSI-approved BSD License (the "License");
+  see accompanying file Copyright.txt for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the License for more information.
+============================================================================*/
 #include "cmState.h"
 
 #include "cmake.h"

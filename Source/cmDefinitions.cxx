818820153b43578eebf9d4c5295423e6adf1f787
Stephen Kelly <steveire@gmail.com>
2015-04-26 15:08:30 +0200

    cmDefinitions: Inline GetInternal into Get.

--- Source/cmDefinitions.cxx
+++ Source/cmDefinitions.cxx
@@ -18,7 +18,7 @@ cmDefinitions::cmDefinitions(cmDefinitions* parent)
 }
 
 //----------------------------------------------------------------------------
-std::pair<const char*, bool> cmDefinitions::GetInternal(const std::string& key)
+std::pair<const char*, bool> cmDefinitions::Get(const std::string& key)
 {
   MapType::const_iterator i = this->Map.find(key);
   std::pair<const char*, bool> result((const char*)0, false);
@@ -27,12 +27,6 @@ std::pair<const char*, bool> cmDefinitions::GetInternal(const std::string& key)
     result = std::make_pair(i->second.Exists ? i->second.c_str() : 0, true);
     }
   return result;
-}
-
-//----------------------------------------------------------------------------
-std::pair<const char*, bool> cmDefinitions::Get(const std::string& key)
-{
-  return this->GetInternal(key);
 }
 
 //----------------------------------------------------------------------------

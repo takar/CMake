a56334284c9753101f0236c07b23f7b38bc8a98e
Stephen Kelly <steveire@gmail.com>
2015-04-26 15:09:11 +0200

    cmDefinitions: Use map::operator[] in Get.
    The key is not already in the map.

--- Source/cmDefinitions.cxx
+++ Source/cmDefinitions.cxx
@@ -43,7 +43,7 @@ const char* cmDefinitions::GetInternal(const std::string& key)
   for (std::vector<cmDefinitions*>::const_iterator it = ups.begin();
        it != ups.end(); ++it)
     {
-    (*it)->Map.insert(MapType::value_type(key, def));
+    (*it)->Map[key] = def;
     }
   return def.Exists? def.c_str() : 0;
 }
